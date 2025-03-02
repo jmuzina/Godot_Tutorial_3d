3f28ca51bae0b234dfeee82e68ab3383efe8cc26
Julie Muzina <julie.muzina@canonical.com>
2025-03-01 23:33:05 -0500

    it moves

--- Player.gd
+++ Player.gd
@@ -26,3 +26,15 @@ func _physics_process(delta: float) -> void:
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
 		$Pivot.basis = Basis.looking_at(direction)
+		
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
